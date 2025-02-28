7247c9ffad4d6e891f2e88bdaaf613c124e66e49
satyav <vsvprasad@gmail.com>
2025-02-27 23:00:51 -0800

    init

--- src/main/resources/install_txt.py
+++ src/main/resources/install_txt.py
@@ -5,3 +5,12 @@ sudo apt-get update
 sudo apt-get install python3-pip
 sudo python3 -m pip install --upgrade pip setuptools wheel
 
+
+pip3 install adafruit-circuitpython-dht
+
+sudo apt-get install libgpiod2
+
+sudo pip3 install adafruit-circuitpython-dht
+
+
+
